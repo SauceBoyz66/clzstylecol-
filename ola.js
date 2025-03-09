7f81b00407493fce1f04466508ec84384edb7c06
темная ночь <losadaartunduaganicolas@gmail.com>
2025-03-09 17:06:24 -0600

    Delete ola.js

--- ola.js
+++
@@ -1,5 +0,0 @@
-function agregarAlCarrito(productoId) {
-    // Aquí puedes agregar la lógica para almacenar los productos en el carrito
-    console.log(`Producto ${productoId} agregado al carrito`);
-    // Por ejemplo, podrías guardar los productos en el localStorage o enviar una solicitud AJAX al servidor.
-}
